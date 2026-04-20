3d5f6dd23af799bd12591ec650b0ae530f8f5e56
BT_C09_10 <mobileappdev@bths.edu>
2026-04-20 14:22:33 -0400

    Added Extra File

---
+++ T30DK/AnothaOne.swift
@@ -0,0 +1,18 @@
+//
+//  AnothaOne.swift
+//  T30DK
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+struct AnothaOne: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnothaOne()
+}
