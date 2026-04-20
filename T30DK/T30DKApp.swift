c9e93f377671130cb1c78c4e9dcee63d4fad7383
BT_C09_10 <mobileappdev@bths.edu>
2026-04-20 14:14:35 -0400

    Initial Commit

---
+++ T30DK/T30DKApp.swift
@@ -0,0 +1,17 @@
+//
+//  T30DKApp.swift
+//  T30DK
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct T30DKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
