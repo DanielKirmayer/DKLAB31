f66eb1909eb299700e7549d98fc4117f373a0dcb
BT_C09_10 <mobileappdev@bths.edu>
2026-04-20 14:20:15 -0400

    Added SampleView

---
+++ T30DK/SampleView.swift
@@ -0,0 +1,18 @@
+//
+//  SampleView.swift
+//  T30DK
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text("Hello Wungus!")
+    }
+}
+
+#Preview {
+    SampleView()
+}
